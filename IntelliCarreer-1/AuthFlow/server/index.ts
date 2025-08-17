d8452a16f425405e75e249d64eadb40edeac7ead
Akshit Shetty <102954215+akshit-shetty@users.noreply.github.com>
2025-08-18 00:10:48 +0530

    Update index.ts

--- IntelliCarreer-1/AuthFlow/server/index.ts
+++ IntelliCarreer-1/AuthFlow/server/index.ts
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config({ path: './googleAuth.env' });
 
 import express, { Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
